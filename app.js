8ceb69c563893ec3854c0961d0986902ac54a167
Jason-bolt <appiatujason@gmail.com>
2022-11-16 23:49:07 +0000

    Added index file

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const path = require('path')
-
+const handlebars = require('express-handlebars')
+// Routes
+const publicRoute = require('./routes/public')
 
 // Load config variables
 dotenv.config({ path: './config/config.env' })
@@ -9,6 +11,22 @@ dotenv.config({ path: './config/config.env' })
 // Initializing app
 const app = express()
 
+// Routes implementation
+app.use('/', publicRoute)
+
+// Handlebars
+app.engine('.hbs', handlebars.engine({
+    helpers: {
+        // formatDate,
+        // stripTags,
+        // truncate,
+        // editIcon,
+        // select
+    },
+    extname: '.hbs',
+    // defaultLayout: 'main_template'
+}))
+app.set('view engine', '.hbs')
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
