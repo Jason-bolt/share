1a095b9043fec1c0f56ec34aafee8db631482b77
Jason-bolt <appiatujason@gmail.com>
2022-11-16 21:24:49 +0000

    Configuring tailwind and started server

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ dotenv.config({ path: './config/config.env' })
 
 // Initializing app
 const app = express()
+
+PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./views/**/*.{hbs, html, js}"],
   theme: {
     extend: {},
   },
