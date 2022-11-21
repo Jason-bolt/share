b3b7689978cee03ae449c1f9495334ebd9527bc7
Jason-bolt <appiatujason@gmail.com>
2022-11-21 12:19:02 +0000

    Added register and login routes, now working on page

--- routes/public.js
+++ routes/public.js
@@ -8,5 +8,10 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+// @desc    Registration page
+// @route   GET /register
+router.get('/register', (req, res) => {
+    res.render('register')
+})
 
 module.exports = router
