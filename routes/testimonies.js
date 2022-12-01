999f743d9e3dc904566192459912f127e7326085
Jason-bolt <appiatujason@gmail.com>
2022-12-01 04:44:00 +0000

    Validating edit form

--- routes/testimonies.js
+++ routes/testimonies.js
@@ -55,10 +55,15 @@ router.delete('/:id', isAuthUser, async (req, res) => {
 // @route   PUT /testimony/:id
 router.put('/:id', isAuthUser, async (req, res) => {
     try {
-        let testimony = await Testimony.findById(req.params.id).exec()
+        let testimony = await Testimony.findById(req.params.id)
 
         if (!testimony){
             return res.render('errors/404')
+        }
+
+        if (req.body.testimony.trim() == ''){
+            req.flash('error', 'Testimony field cannot be left blank!')
+            res.redirect('/profile')
         }
 
         if (String(testimony.user) != String(req.user._id)){
